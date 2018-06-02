2c8e1f57bb23e82a8abf206d7a0ca484da752fef
DeafeningRoar <finalx7@gmail.com>
2018-06-01 22:08:03 -0400

    Add ruta remover oferta

--- app/models/oferta/private.routes.js
+++ app/models/oferta/private.routes.js
@@ -6,6 +6,6 @@ const app = express.Router()
 app.route('/oferta/:id([0-9]+)?')
   .post   (permit(214), (req,res) => controller.POST(req, res))
   .put    (permit(214), (req,res) => controller.PUT(req, res))
-  // .delete ((req,res) => controller.DELETE(req, res))
+  .delete (permit(214), (req,res) => controller.DELETE(req, res))
 
 export default app
