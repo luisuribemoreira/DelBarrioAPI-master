8e61efae691c4159b35f1278dc6f88d8dc3acd10
DeafeningRoar <finalx7@gmail.com>
2018-08-24 19:08:10 -0300

    Metodo para cambiar a vigente las ofertas cuya fecha de inicio se alcanzó

--- scheduledtasks.js
+++ scheduledtasks.js
@@ -2,7 +2,7 @@ import cron from 'node-cron'
 import Ofertas from './app/models/oferta/model'
 import moment from 'moment'
 
-// Se ejecuta todos los dias a las 12:00 AM
+// Se ejecuta todos los dias a las 12:00
 // Cambia el FLAG_VIGENTE de true a false
 // en las ofertas que ya expiraron.
 // Las cambia al dia siguiente del termino. Ej; Si termina el 11, el 12 se cambia a false.
@@ -24,6 +24,38 @@ cron.schedule('0 0 0 * * *', function(){
                 FLAG_VALIDADO:    oferta.FLAG_VALIDADO,
                 FLAG_BAN:         oferta.FLAG_BAN,
                 FLAG_VIGENTE:     false
+              }).catch(err => {
+                console.log(err)
+              })
+            })
+        }
+      })
+    }).catch(err => {
+      console.log(err)
+    })
+})
+
+/*
+* Cambia las ofertas a vigentes una vez la fecha de inicio es igual a la fecha actual.
+*/
+cron.schedule('0 0 0 * * *', function(){
+  new Ofertas.Collection().fetch({})
+    .then(entities => {
+      let ofertas = entities.toJSON()
+      ofertas.forEach(oferta => {
+        let fechaActual = moment().format('DD-MM-YYYY')
+        let FECH_INICIO = moment(oferta.FECH_INICIO).format('DD-MM-YYYY')
+        if (fechaActual === FECH_INICIO) {
+          new Ofertas.Model({IDEN_OFERTA: oferta.IDEN_OFERTA}).fetch({require: true})
+            .then(entity => {
+              entity.save({
+                IDEN_PUBLICACION: oferta.IDEN_PUBLICACION,
+                FECH_INICIO:      oferta.FECH_INICIO,
+                FECH_TERMINO:     oferta.FECH_TERMINO,
+                NUMR_PRECIO:      oferta.NUMR_PRECIO,
+                FLAG_VALIDADO:    oferta.FLAG_VALIDADO,
+                FLAG_BAN:         oferta.FLAG_BAN,
+                FLAG_VIGENTE:     true
               }).catch(err => {
                 console.log(err)
               })
