7da9bd7fbe2d25e274b37fa34360fb7ee92f6720
DeafeningRoar <finalx7@gmail.com>
2018-08-26 16:36:41 -0300

    Se agrega una ruta publica para contactos por persona

--- app/models/contacto/public.routes.js
+++ app/models/contacto/public.routes.js
@@ -3,6 +3,6 @@ import express from 'express'
 const app = express.Router()
 
 app.route('/contacto/:id([0-9]+)?')
-  .get    ((req,res) => controller.GET(req, res))
+  .get    ((req,res) => controller.GETByPersona(req, res))
 
 export default app
