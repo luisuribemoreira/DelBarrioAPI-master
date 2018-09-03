f0d5585593dc74262f740d2c12e60007e7a1d444
DeafeningRoar <finalx7@gmail.com>
2018-09-03 20:47:35 -0300

    Se crea metodo para enviar correo con attachments y se aplican variables de entorno al correo.

--- app/specific/emailer.js
+++ app/specific/emailer.js
@@ -1,22 +1,23 @@
 import nodemailer from 'nodemailer'
 import express from 'express'
+import config from '../config'
 
 const app = express.Router()
 
 let transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'delbarriotest@gmail.com',
-    pass: 'delbarrio1234'
+    user: config.mail,
+    pass: config.mailPsw
   }
 })
 
 function send (req, res) {
   transporter.sendMail({
-    from: 'delbarriotest@gmail.com',
+    from: config.mail,
     to: req.body.to,
     subject: req.body.subject,
-    text: req.body.text
+    text: req.body.text,
   }, (errors, info) => {
     if (errors) {
       res.status(500).json({error: true, data: {message: 'Internal error', error: errors}})
@@ -26,7 +27,31 @@ function send (req, res) {
   })
 }
 
+function sendWithAttachment(options) {
+  transporter.sendMail({
+    from: 'delbarriotest@gmail.com',
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+    attachments: [
+      {
+        filename: options.fileName,
+        path: options.path
+      }
+    ]
+  }, (errors, info) => {
+    if (errors) {
+      console.log(errors)
+    } else {
+      console.log(info)
+    }
+  })
+}
+
 app.route('/email')
   .post((req, res) => send(req, res))
 
-export default app
+export default {
+  app,
+  sendWithAttachment
+}
