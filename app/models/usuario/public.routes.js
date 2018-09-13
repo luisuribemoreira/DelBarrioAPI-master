51559e2c84d610cf749540a1836247a66a7a3329
DeafeningRoar <finalx7@gmail.com>
2018-09-13 17:39:33 -0300

    Se elimina ruta publica para ver usuarios por desuso.

--- app/models/usuario/public.routes.js
+++
@@ -1,9 +0,0 @@
-import controller from './controller'
-import express from 'express'
-const app = express.Router()
-
-app.route('/usuario/:id([0-9]+)?')
-  .get    ((req,res) => controller.GET(req, res))
-  .post   ((req,res) => controller.POST(req, res))
-
-export default app
