4f2ce6e605204944fe517e8dd78dfcaf322dfe75
Sebastián Feliú <sebastianfeliu97@gmail.com>
2019-03-05 18:42:23 -0300

    Se modifica el middleware de permisos para poder comparar por un arreglo de permisos si es que varios tipos de usuarios necesitan ejecutar un ruta

--- app/middlewares/permission.js
+++ app/middlewares/permission.js
@@ -9,11 +9,40 @@ function permit (allowed) {
     req.user.rol.permisos.forEach(element => {
       arrayPermisos.push(element.CODI_PERMISO)
     });
-     if (req.user && arrayPermisos.includes(allowed))
-       next() // Si el codigo del permiso lo mantiene el usuario pasa al siguiente middleware
-     else {
-       res.status(403).json({ error: true, data: { message: 'Forbidden' } })
-     }
+    // Se declara un Flag para saber si el codigo del permiso lo mantiene el usuario en caso de ser un Arreglo
+    var auxMid = false
+    // Se pregunta si la variable que viene del metodo es un arreglo o solo un número
+    // Se implementa un arreglo ya que varios usuarios pueden ejecutar una ruta pero mantienen cddigo de diferente
+    // Por ejemplo el administrador , super administrador y emprendendedor pueden editar informacion
+    // de un emprendedor , siendo el caso que el emprendedor solo corresponde a sus datos
+    if(Array.isArray(allowed)) {
+      // Se recorre el arreglo de codigos de permiso que pueden efectuar la ruta solicitada
+      for (let i = 0; i < allowed.length; i++) {
+        if(arrayPermisos.includes(allowed[i])) {
+          console.log(allowed[i])
+          // Si existe la variable el flag cambia a true y se detiene el ciclo for
+          auxMid = true
+          break;
+        }      
+      }
+      // Si el flag es verdadero se pasa al siguiente middleware
+      if (req.user && auxMid) {
+        next();
+      } else {
+        // Si no se prohibe el acceso
+        res.status(403).json({ error: true, data: { message: 'Forbidden' } })
+      }
+    } else if(Number.isInteger(allowed)){
+      if (req.user && arrayPermisos.includes(allowed)) {
+        next() // Si el codigo del permiso lo mantiene el usuario pasa al siguiente middleware
+      } else {
+        res.status(403).json({ error: true, data: { message: 'Forbidden' } })
+      }
+    } else { 
+        res.status(403).json({ error: true, data: { message: 'Forbidden' } })
+
+    }
+
   }
 }
 
--- app/models/emprendedor/private.routes.js
+++ app/models/emprendedor/private.routes.js
@@ -4,9 +4,9 @@ import permit from '../../middlewares/permission'
 const app = express.Router()
 
 app.route('/emprendedor/:id([0-9]+)?')
-  .get    (permit(103), (req,res) => controller.GET(req, res)) // Revisar
+  .get    (permit(103), (req,res) => controller.GET(req, res))
   .post   (permit(225), (req,res) => controller.POST(req, res))
-  .put    ((req,res) => controller.PUT(req, res)) // Pendiente
+  .put    (permit([207,210]), (req,res) => controller.PUT(req, res))
   // .delete ((req,res) => controller.DELETE(req, res))
 
 export default app
