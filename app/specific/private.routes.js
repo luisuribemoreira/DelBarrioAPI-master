f54153273c6ef39860022ccd990729d8a79ad80a
SebastianFeliu <sebastianfeliu97@gmail.com>
2019-03-05 22:07:27 -0300

    Se agregan codigos del sistema de permisos a las rutas de denuncia y administrar clientes e emprendedores

--- app/specific/private.routes.js
+++ app/specific/private.routes.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import clientes from './private/clientes'
 import administradores from './private/administradores'
+import permit from '../middlewares/permission'
 
 const app = express.Router()
 
 app.route('/clientes')
-  .get   ((req,res) => clientes.GET(req, res))
+  .get   (permit(209),(req,res) => clientes.GET(req, res))
 
 app.route('/administradores')
-  .get   ((req,res) => administradores.GET(req, res))
+  .get   (permit(211),(req,res) => administradores.GET(req, res))
 
 export default app
