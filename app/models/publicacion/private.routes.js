dce2cb6a2dc3eb4fd028d11217c09801b407efce
SebastianFeliu <sebastianfeliu97@gmail.com>
2019-10-31 01:03:09 -0300

    Fix en aceptación de publicaciones

--- app/models/publicacion/private.routes.js
+++ app/models/publicacion/private.routes.js
@@ -4,8 +4,8 @@ import permit from '../../middlewares/permission'
 const app = express.Router()
 
 app.route('/publicacion/:id([0-9]+)?')
-  .post   (permit(212), (req,res) => controller.POST(req, res))
-  .put    (permit(212), (req,res) => controller.PUT(req, res))
+  .post   (permit([212,213]), (req,res) => controller.POST(req, res))
+  .put    (permit([212,213]), (req,res) => controller.PUT(req, res))
   // .delete ((req,res) => controller.DELETE(req, res))
 
 export default app
