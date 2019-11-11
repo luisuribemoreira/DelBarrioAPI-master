641e1ac573099056f053f92c43e0877b3ac4abba
SebastianFeliu <sebastianfeliu97@gmail.com>
2019-11-10 21:49:24 -0300

    Se elimina verificacion de rutas inncesarias por ser privadas

--- app/models/calificacion/private.routes.js
+++ app/models/calificacion/private.routes.js
@@ -6,8 +6,8 @@ const app = express.Router()
 
 app.route('/calificacion/:id([0-9]+)?')
   .get    (permit(106), (req,res) => controller.GET(req, res))
-  .post   (permit(220), inject.IDEN_USUARIO(), (req,res) => controller.POST(req, res))
-  .put    (permit(221), (req,res) => controller.PUT(req, res))
+  .post   (inject.IDEN_USUARIO(), (req,res) => controller.POST(req, res))
+  .put    ((req,res) => controller.PUT(req, res))
   // .delete ((req,res) => controller.DELETE(req, res))
 
 export default app
--- app/models/comentario/private.routes.js
+++ app/models/comentario/private.routes.js
@@ -6,8 +6,8 @@ const app = express.Router()
 
 app.route('/comentario/:id([0-9]+)?')
   .get    (permit(104), (req,res) => controller.GET(req, res))
-  .post   (permit(216), inject.IDEN_USUARIO(), (req,res) => controller.POST(req, res))
-  .put    (permit(218), (req,res) => controller.PUT(req, res))
+  .post   (inject.IDEN_USUARIO(), (req,res) => controller.POST(req, res))
+  .put    ((req,res) => controller.PUT(req, res))
   // .delete ((req,res) => controller.DELETE(req, res))
 
 export default app
